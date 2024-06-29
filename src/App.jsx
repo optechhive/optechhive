c2b13fa52489655bd292d68499b537c95c0fc533
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-06-29 11:55:01 +0600

    Initial 1st Site Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import optechHiveLogo from "./assets/images/optech-logo-512x512.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="flex flex-col justify-center items-center font-oswald h-[100vh] mx-4">
+            <div className="text-container my-4">
+                <p className="text-xl">Welcome to,</p>
+                <h1 className="text-5xl">Optech Hive</h1>
+            </div>
+            <img
+                src={optechHiveLogo}
+                className="max-w-[256px] w-full rounded-3xl my-4"
+                alt="Optech Hive Logo"
+            />
+            <p className="hover:text-blue-500">
+                A trusted Software Agency
+            </p>
+        </div>
+    );
 }
 
-export default App
+export default App;

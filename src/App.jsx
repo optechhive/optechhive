c9bade0b1e65475edf5cb6bdd4d34c7a2b534764
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-07-02 18:34:17 +0600

    Some minor changes 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,21 @@ function App() {
         <div className="flex flex-col justify-center items-center font-oswald h-[100vh] mx-4">
             <div className="text-container my-4">
                 <p className="text-xl">Welcome to,</p>
-                <h1 className="text-5xl">Optech Hive</h1>
+                <h1 className="text-5xl">Optech Hive - Software Agency</h1>
             </div>
             <img
                 src={optechHiveLogo}
                 className="max-w-[256px] w-full rounded-3xl my-4"
                 alt="Optech Hive Logo"
             />
-            <p className="hover:text-blue-500">
+            <p className="hover:text-blue-500 text-xl mb-1">
                 A trusted Software Agency
+            </p>
+            <p>
+                Email:{" "}
+                <a href="mailto:contact@optechhive.com">
+                    contact@optechhive.com
+                </a>
             </p>
         </div>
     );
